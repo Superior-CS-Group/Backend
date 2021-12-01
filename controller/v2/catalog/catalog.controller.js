4b3327766a5ca149619663aff8b62ecb7435f0d0
ram-digimonk <ram.gautam@digimonk.in>
2021-12-01 11:33:22 +0530

    updated estimate api

--- controller/v2/catalog/catalog.controller.js
+++ controller/v2/catalog/catalog.controller.js
@@ -5,6 +5,8 @@ import {
   validateCreateVariationInput,
 } from "../../../validator/catalog/v2/catalog.js";
 import base64ToFile from "../../../utils/base64ToFile.js";
+import StaffModel from "../../../model/staff/staffModel.js";
+
 export async function createCatalog(req, res) {
   try {
     console.log("req: ", req.user);
@@ -39,13 +41,14 @@ export async function createCatalog(req, res) {
 
 export async function updateCatalog(req, res) {
   try {
-    const catalogId = req.params.catalogId;
+    // const catalogId = req.params.catalogId;
+    console.log(req.body)
     const { isValid, errors } = validateCreateCatalogInput(req.body);
     if (!isValid) {
       return res.status(400).json({ errors });
     }
     const updatedCatalog = await CatalogModel.findByIdAndUpdate(
-      catalogId,
+      {_id:req.body.catelogId},
       {
         $set: req.body,
       },
@@ -61,6 +64,31 @@ export async function updateCatalog(req, res) {
   }
 }
 
+export const RemoveCatelog = async (req, res) => {
+  const userId = req.query.userId || req.user._id;
+  // console.log(userId);
+  const currentUser = await StaffModel.findById(userId);
+
+  if (!currentUser) {
+    return res.status(401).json({ error: "User not found" });
+  }
+  try {
+
+    await VariationModelV2.deleteMany({ catelogId:req.body.id });
+    await CatalogModel.findByIdAndDelete({ _id: req.body.id });
+
+    const checkData = await CatalogModel.find().sort({ _id: -1 });
+
+    res.status(200).json({
+      DataLength: checkData.length,
+      data: checkData,
+    });
+  } catch (error) {
+    console.log("error:", error);
+    res.status(500).json({ msg: "Internal server error" });
+  }
+};
+
 export async function getCatalogs(req, res) {
   try {
     const catalogs = await CatalogModel.find();
@@ -72,6 +100,7 @@ export async function getCatalogs(req, res) {
       .json({ msg: "Internal Server Error", errors: error });
   }
 }
+
 
 export async function createVariation(req, res) {
   try {
@@ -91,6 +120,68 @@ export async function createVariation(req, res) {
       .json({ msg: "New Variation created successfully", data: newVariation });
   } catch (error) {
     console.log(error);
+    return res
+      .status(500)
+      .json({ msg: "Internal Server Error", errors: error });
+  }
+}
+
+export async function updateVariation(req, res) {
+  try {
+    // const variationId = req.params.variationId;
+    const { isValid, errors } = validateCreateCatalogInput(req.body);
+    if (!isValid) {
+      return res.status(400).json({ errors });
+    }
+    const updatedVariation = await VariationModelV2.findByIdAndUpdate(
+      {_id:req.body.id},
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    return res
+      .status(200)
+      .json({ msg: "Variation updated successfully", data: updatedVariation });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ msg: "Internal Server Error", errors: error });
+  }
+}
+
+export const RemoveVariation = async (req, res) => {
+  const userId = req.query.userId || req.user._id;
+  // console.log(userId);
+  const currentUser = await StaffModel.findById(userId);
+
+  if (!currentUser) {
+    return res.status(401).json({ error: "User not found" });
+  }
+  try {
+    await VariationModelV2.findByIdAndDelete({ _id: req.body.id });
+
+    const checkData = await VariationModelV2.find().sort({ _id: -1 });
+
+    res.status(200).json({
+      DataLength: checkData.length,
+      data: checkData,
+    });
+  } catch (error) {
+    console.log("error:", error);
+    res.status(500).json({ msg: "Internal server error" });
+  }
+};
+
+export async function getVariationsData(req, res) {
+  try {
+    const variationId = req.query.variationId;
+    
+    const variations = await VariationModelV2.findById({ _id:req.body.id });
+    console.log("valriations: ", variations);
+    return res.status(200).json({ data: variations });
+  } catch (error) {
+    console.log("error: ", error);
     return res
       .status(500)
       .json({ msg: "Internal Server Error", errors: error });
--- route/v2/catalog.js
+++ route/v2/catalog.js
@@ -7,13 +7,21 @@ import {
   getVariationsByCatalog,
   searchCatalogByName,
   updateCatalog,
+  RemoveCatelog,
+  updateVariation,
+  RemoveVariation,
+  getVariationsData,
 } from "../../controller/v2/catalog/catalog.controller.js";
 
 const CatalogRouteV2 = express.Router();
 
 CatalogRouteV2.post("/create-catalog", authProtect, createCatalog);
 CatalogRouteV2.post("/create-variation", authProtect, createVariation);
-CatalogRouteV2.put("/update-catalog/:catalogId", authProtect, updateCatalog);
+CatalogRouteV2.post("/update-catalog/", updateCatalog);
+CatalogRouteV2.post("/update-variation/", updateVariation);
+CatalogRouteV2.post("/remove-catalog", authProtect, RemoveCatelog);
+CatalogRouteV2.post("/remove-variation", authProtect, RemoveVariation);
+CatalogRouteV2.post("/get-variation", getVariationsData);
 CatalogRouteV2.get("/get-all-catalog", getCatalogs);
 CatalogRouteV2.get("/get-all-variation-by-catalog", getVariationsByCatalog);
 CatalogRouteV2.get("/search-catalog-by-name/:catalogName", searchCatalogByName);
